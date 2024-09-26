280c1ebb9c831f721be749e9a93cf1eaff699c61
sacktools <59923996@qq.com>
2024-09-26 11:41:42 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -14,27 +14,24 @@ self.onmessage = async function (e) {
     const web3 = new Web3(Web3.givenProvider || "https://bsc-dataseed1.defibit.io");
     const routerContract = new web3.eth.Contract(abi, routerContractAddress);
     const path = [tokeninAddress, tokenOutAddress];
-    let failureCount = 0;
-    const gasEstimateInterval = setInterval(async () => {
-        try {
-            const gasLimit = await routerContract.methods.swapExactTokensForTokens(
-                amountIn,
-                0,
-                path,
-                to,
-                deadline
-            ).estimateGas({ from: to });
 
-            if (gasLimit) {
-                clearInterval(gasEstimateInterval); // 只需清除一次
+    self.onmessage = async (e) => {
+        const { command } = e.data;
+        if (command === 'estimateGas') {
+            try {
+                const gasLimit = await routerContract.methods.swapExactTokensForTokens(
+                    amountIn,
+                    0,
+                    path,
+                    to,
+                    deadline
+                ).estimateGas({ from: to });
+
+                // 返回有效的 gas limit
                 postMessage({ type: 'estimateGas', message: { gasLimit } });
-                
+            } catch (error) {
+                postMessage({ type: 'estimateGas', message: { gasLimit: 0 } });
             }
-        } catch (error) {
-            postMessage({ type: 'log', message: { text: '交易未开启，进行第 ' + failureCount + ' 次检查', color: 'blue' } });
-            postMessage({ type: 'estimateGas', message: { gasLimit: 0 } }); // 返回无效 gasLimit
-            failureCount++;
         }
-    }, 3000);
-
+    };
 };
